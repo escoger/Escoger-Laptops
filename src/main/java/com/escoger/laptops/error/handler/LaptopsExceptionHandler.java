b3d09245325241590c964a561dec9f8f3c0dfe8f
gaurkuma <gaurav.kumar8@hpe.com>
2018-11-17 15:15:15 +0530

    changes

--- src/main/java/com/escoger/laptops/error/handler/LaptopsExceptionHandler.java
+++
@@ -1,7 +0,0 @@
-package com.escoger.laptops.error.handler;
-
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-public class LaptopsExceptionHandler extends ResponseEntityExceptionHandler{
-
-}
